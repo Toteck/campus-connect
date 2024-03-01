7831824d015682f2d29c8009cf8dd3762c4f2dd8
mateus <mateusweslley14@gmail.com>
2024-02-29 21:56:13 -0300

    Alteração do método store de ClassController

--- app/Controllers/Http/ClassesController.ts
+++ app/Controllers/Http/ClassesController.ts
@@ -1,10 +1,24 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
 import Class from 'App/Models/Class'
 import CreateClassValidator from 'App/Validators/CreateClassValidator'
 
 export default class ClassesController {
   public async store({ request, response }: HttpContextContract) {
     const classPayload = await request.validate(CreateClassValidator)
+
+    // Verificando se existe outra turma com os mesmos dados
+    const existingClass = await Class.query()
+      .where('name', classPayload.name)
+      .where('year', classPayload.year)
+      .where('period', classPayload.period)
+      .where('shift', classPayload.shift)
+      .where('course_id', classPayload.courseId)
+      .first()
+
+    if (existingClass) {
+      throw new BadRequestException('Another class with the same data already exists', 409)
+    }
 
     const classe = await Class.create(classPayload)
     await classe.load('courseClass')
--- tests/functional/classes/classes.spec.ts
+++ tests/functional/classes/classes.spec.ts
@@ -12,7 +12,7 @@ test.group('Classes', (group) => {
     // Para mim criar uma turma eu preciso criar um curso antes
     const coursePayload = {
       degree: 'superior',
-      name: 'Sistemas para Internet',
+      name: 'Sistemas de Internet',
     }
 
     const response = await client.post('/course').json(coursePayload)
@@ -32,5 +32,35 @@ test.group('Classes', (group) => {
 
     response2.assertStatus(201)
     console.log(response2.body().classe)
+  })
+
+  test('it should try to create a class with data being used by another class', async ({
+    client,
+    assert,
+  }) => {
+    // Para mim criar uma turma eu preciso criar um curso antes
+    const coursePayload = {
+      degree: 'superior',
+      name: 'Sistemas de Internet',
+    }
+
+    const response = await client.post('/course').json(coursePayload)
+    response.assertStatus(201)
+    const course = response.body().course
+
+    // Criação da turma
+    const classPayload = {
+      name: 'Tec. em Sistemas de Internet 2022.2',
+      year: '2022',
+      period: '2',
+      shift: 'vespertino',
+      courseId: course.id,
+    }
+
+    const response2 = await client.post('/classes').json(classPayload)
+    response2.assertStatus(201)
+    const response3 = await client.post('/classes').json(classPayload)
+
+    console.log(response3.body())
   })
 })
