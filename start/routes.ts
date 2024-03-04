61be0cfeaf191649d61f46ca0b0e38ecba937135
mateus <mateusweslley14@gmail.com>
2024-03-04 12:00:08 -0300

    Autenticação colocada nas rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,28 +1,31 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+// Session
+Route.post('/sessions', 'SessionsController.store') // Login
 // User
 Route.post('/users', 'UsersController.store')
 
-// Session
-Route.post('/sessions', 'SessionsController.store')
-Route.delete('/sessions', 'SessionsController.destroy')
+Route.group(() => {
+  // Session
+  Route.delete('/sessions', 'SessionsController.destroy').middleware('auth')
 
-// Course
-Route.post('/course', 'CoursesController.store')
-Route.get('/course/:id', 'CoursesController.show')
-Route.get('/course', 'CoursesController.index')
-Route.get('/course/:id/classes', 'CoursesController.classesByCourse')
-Route.patch('/course/:id', 'CoursesController.update')
-Route.delete('/course/:id', 'CoursesController.destroy')
+  // Course
+  Route.post('/course', 'CoursesController.store').middleware('auth')
+  Route.get('/course/:id', 'CoursesController.show')
+  Route.get('/course', 'CoursesController.index')
+  Route.get('/course/:id/classes', 'CoursesController.classesByCourse')
+  Route.patch('/course/:id', 'CoursesController.update')
+  Route.delete('/course/:id', 'CoursesController.destroy')
 
-// Classes
-Route.post('/classes', 'ClassesController.store')
-Route.patch('/classes/:id', 'ClassesController.update')
-Route.get('/classes/:id', 'ClassesController.show')
-Route.get('/classes', 'ClassesController.index')
+  // Classes
+  Route.post('/classes', 'ClassesController.store')
+  Route.patch('/classes/:id', 'ClassesController.update')
+  Route.get('/classes/:id', 'ClassesController.show')
+  Route.get('/classes', 'ClassesController.index')
 
-// Events
-Route.post('/events', 'EventsController.store')
-Route.patch('/events/:id', 'EventsController.update')
-Route.get('/events', 'EventsController.index')
-Route.delete('/events/:id', 'EventsController.destroy')
+  // Events
+  Route.post('/events', 'EventsController.store')
+  Route.patch('/events/:id', 'EventsController.update')
+  Route.get('/events', 'EventsController.index')
+  Route.delete('/events/:id', 'EventsController.destroy')
+}).middleware('auth')
